67644b61aadd00732628a42d187a11f87df4e519
MoketeTyabekana <75148610+MoketeTyabekana@users.noreply.github.com>
2024-01-27 01:19:53 +0200

    Removed The Cardview round corners. Added a New Activity For Password Reset.

---
+++ app/src/main/java/com/example/myapplication/Password_Reset.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Password_Reset extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_reset);
+    }
+}

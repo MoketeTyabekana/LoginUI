4be9b4a30ff2bb211a542a6f001685318b27e35a
MoketeTyabekana <75148610+MoketeTyabekana@users.noreply.github.com>
2024-01-27 02:05:33 +0200

    Added New Activities, Reset Password and Registration Activity.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,15 +1,33 @@
 package com.example.myapplication;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvReset,tvAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tvReset=findViewById(R.id.tvReset);
+        tvAccount=findViewById(R.id.tvAccount);
 
+
+        tvReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intentReset=new Intent(MainActivity.this,Password_Reset.class);
+                startActivity(intentReset);
+            }
+        });
     }
 }
